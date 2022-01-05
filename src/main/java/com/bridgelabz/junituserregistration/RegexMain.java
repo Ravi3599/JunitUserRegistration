14d3686c53afb51e52b88dbc611199071e23f924
Ravi3599 <ravikantdukre2018@gmail.com>
2022-01-05 14:38:50 +0530

    UPDATED: validation of phone number

--- src/main/java/com/bridgelabz/junituserregistration/RegexMain.java
+++ src/main/java/com/bridgelabz/junituserregistration/RegexMain.java
@@ -15,5 +15,8 @@ import java.util.Scanner;
 			System.out.println("Enter Email: ");
 			String mail = sc.next();
 			registration.validateEmail(mail);
+			System.out.println("Enter Phone Number: ");
+			String number = sc.next();
+			registration.validatePhoneNumber(number);
 		}
 }
