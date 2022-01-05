5873c0165a37279ecfd894ac1924fcc32cf9c3b6
Ravi3599 <ravikantdukre2018@gmail.com>
2022-01-05 14:03:05 +0530

    UPDATED: test case for validating last name

--- src/test/java/com/bridgelabz/junituserregistration/UserDetailsTest.java
+++ src/test/java/com/bridgelabz/junituserregistration/UserDetailsTest.java
@@ -4,24 +4,36 @@ import org.junit.Assert;
 import org.junit.Test;
 
 public class UserDetailsTest {
-	
+	UserRegistration register = new UserRegistration();
+
 	@Test
 	public void givenFirstName_WhenProper_ShouldReturnTrue() {
-		UserRegistration register = new UserRegistration();
 		boolean isValid = register.validateFirstName("Ravi");
 		Assert.assertTrue(isValid);
 	}
 	@Test
 	public void givenFirstName_WhenNotProper_ShouldReturnFalse() {
-		UserRegistration register = new UserRegistration();
 		boolean isValid = register.validateFirstName("ravi");
 		Assert.assertFalse(isValid);
 	}
 	@Test
 	public void givenFirstName_WhenShort_ShouldReturnFalse() {
-		UserRegistration register = new UserRegistration();
 		boolean isValid = register.validateFirstName("ra");
 		Assert.assertFalse(isValid);
 	}
-	
+	@Test
+	public void givenLastName_WhenProper_ShouldReturnTrue() {
+		boolean isValid = register.validateFirstName("Kumar");
+		Assert.assertTrue(isValid);
+	}
+	@Test
+	public void givenLastName_WhenNotProper_ShouldReturnFalse() {
+		boolean isValid = register.validateFirstName("kumar");
+		Assert.assertFalse(isValid);
+	}
+	@Test
+	public void givenLastName_WhenShort_ShouldReturnFalse() {
+		boolean isValid = register.validateFirstName("ku");
+		Assert.assertFalse(isValid);
+	}
 }
