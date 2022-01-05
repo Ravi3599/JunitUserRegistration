41e0e7788c6897399799a2fa59c49cbb5e40e8b4
Ravi3599 <ravikantdukre2018@gmail.com>
2022-01-05 22:22:13 +0530

    UPDATED: email regex

--- src/main/java/com/bridgelabz/junituserregistration/UserRegistration.java
+++ src/main/java/com/bridgelabz/junituserregistration/UserRegistration.java
@@ -5,7 +5,7 @@ import java.util.regex.*;
 public class UserRegistration {
 		private static final String FIRST_NAME_REGEX = "^[A-Z][a-z]{2,}";
 		private static final String LAST_NAME_REGEX = "^[A-Z][a-z]{2,}$";
-		private static final String EMAIL_REGEX = "^[a-zA-Z0-9_-]+(?:\\.[a-zA-Z0-9_+-]+)*@[a-zA-Z0-9-]+(?:\\.[a-zA-Z0-9-]+)*$";
+		private static final String EMAIL_REGEX = "^[a-zA-Z0-9_+-]+(?:\\.[a-zA-Z0-9_+-]+)*@[a-zA-Z0-9-]+(?:\\.[a-zA-Z0-9-]{2,})*(?![a-zA-Z0-9.]+)*$";
 		private static final String PHONE_NUMBER_REGEX = "^[0-9]{2}[0-9]{10}";
 		private static final String PASSWORD_REGEX = "(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%^&*()_-])[a-zA-Z0-9].{8,}$";
 		static boolean isFirstNameValid,isLastNameValid,isMailValid,isNumberValid,isPasswordValid,isMoodValid;
